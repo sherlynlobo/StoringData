b5534571c1ff68873ece20b0225f7ea8595a4e19
sherlynlobo <sherlynlobo26@gmail.com>
2018-12-12 23:14:34 -0500

    Made UI for the app

--- StoringData/ViewController.swift
+++ StoringData/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    
+    @IBOutlet weak var output: UILabel!
+    
+    
+    @IBAction func submit(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
